c16f26112bafaede9394ab44da552ffec934ae7d
Ilya <cyberiliy.25@mail.ru>
2023-07-05 09:41:42 +0600

    correlation matrix

--- analyze.py
+++ analyze.py
@@ -9,3 +9,10 @@ import plotly.figure_factory as ff
 df = pd.read_csv("heart_failure_clinical_records_dataset.csv")
 print(df)
 print(df.describe().T)
+
+cmap = sns.diverging_palette(2, 165, s=80, l=55, n=9)
+corrmat = df.corr()
+plt.subplots(figsize=(20,20))
+sns.heatmap(corrmat,cmap= cmap,annot=True, square=True)
+plt.show()
+
