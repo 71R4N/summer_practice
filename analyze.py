c04045aef12883b7e5106f84d78299dc4c310250
Ilya <cyberiliy.25@mail.ru>
2023-07-05 09:48:19 +0600

    added 2 charts

--- analyze.py
+++ analyze.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
 import plotly.figure_factory as ff
+import scipy
 
 df = pd.read_csv("heart_failure_clinical_records_dataset.csv")
 print(df)
@@ -16,3 +17,13 @@ plt.subplots(figsize=(20,20))
 sns.heatmap(corrmat,cmap= cmap,annot=True, square=True)
 plt.show()
 
+hist_data =[df["age"].values]
+group_labels = ['age']
+fig = ff.create_distplot(hist_data, group_labels)
+fig.update_layout(title_text='Age Distribution plot')
+fig.show()
+
+fig = px.box(df, x='sex', y='age', points="all")
+fig.update_layout(
+    title_text="Gender wise Age Spread - Male = 1 Female =0")
+fig.show()
